f40fc24388afa172684b4608a651c1817d5d4d86
王开智 <kidswong999@163.com>
2018-06-25 12:04:15 +0800

    Update main.py

--- pca9685/main.py
+++ pca9685/main.py
@@ -1,14 +1,15 @@
 import time
+from machine import Pin, I2C
 from servo import Servos
-from machine import I2C, Pin
 
-i2c = I2C(sda=Pin('P5'), scl=Pin('P4'))
-servo = Servos(i2c, address=0x40, freq=50, min_us=650, max_us=2800, degrees=180)
+i2c = I2C(sda=Pin(21), scl=Pin(22))#moxing esp32
+#i2c = I2C(sda=Pin('P26'), scl=Pin('P25'))#moxing stm32
+servo = Servos(i2c, address=0x40, freq=50, min_us=500, max_us=2500, degrees=180)
 
 while True:
-    for i in range(0, 8):
+    for i in range(0, 16):
         servo.position(i, 0)
-    time.sleep(500)
-    for i in range(0, 8):
+    time.sleep_ms(500)
+    for i in range(0, 16):
         servo.position(i, 180)
-    time.sleep(500)
+    time.sleep_ms(500)
